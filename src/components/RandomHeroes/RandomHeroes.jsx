d5fa07f71408decbbd9576f6e6d51d11c76c4195
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-03 22:55:50 +0100

    commit

--- src/components/RandomHeroes/RandomHeroes.jsx
+++ src/components/RandomHeroes/RandomHeroes.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { fetchRandomHeroes } from "../../services/api";
 
 const RandomHeroes = () => {
   const [randomHeroes, setRandomHeroes] = useState([]);
-  const handleBtnClick = () => {
+  const loadHeroes = () => {
     Promise.all([
       fetchRandomHeroes(),
       fetchRandomHeroes(),
@@ -11,10 +11,15 @@ const RandomHeroes = () => {
     ]).then((data) => setRandomHeroes(data));
   };
   console.log(randomHeroes);
+
+  useEffect(() => {
+    loadHeroes();
+  }, []);
+
   return (
     <div>
       <h1>Three Random Superheroes</h1>
-      <button onClick={handleBtnClick}>Generate</button>
+      {/* <button onClick={loadHeroes}>Generate</button> */}
       <ul>
         {randomHeroes.map((hero) => (
           <li key={hero.id}>{hero.name}</li>
