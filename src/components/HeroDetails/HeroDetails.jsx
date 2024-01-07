f4d64390f8171572c410505bebdd9ad912c7bb5a
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-07 17:07:12 +0100

    commit

--- src/components/HeroDetails/HeroDetails.jsx
+++ src/components/HeroDetails/HeroDetails.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const HeroDetails = () => {
   const { heroId } = useParams();
-
+  console.log(heroId);
   return (
     <div>
       <h1>Details for Hero {heroId}</h1>
